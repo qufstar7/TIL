392049b1b2a8e02ced1d1fd73c6d52fbfe12ab95
qufstar7 <57350342+qufstar7@users.noreply.github.com>
2021-09-01 19:05:27 +0900

    210901 TIL how to configure css, js code in html(directory configuration)

---
+++ Web programming(full stack)/3. web app development/directory/js/common.js
@@ -0,0 +1,3 @@
+function foo() {
+  console.log("메세지를 출력하는 푸 함수 추가");
+}
---
+++ Web programming(full stack)/3. web app development/directory/js/main.js
@@ -0,0 +1,4 @@
+console.log("hello!!");
+var elHeading = document.querySelector("h1");
+console.log(elHeading);
+foo(elHeading);
