65ecdcbcc19c3c1ed4ab7c56dacb72f614be303a
qufstar7 <57350342+qufstar7@users.noreply.github.com>
2022-01-20 19:01:34 +0900

    220120 폴더 카테고리 변경 (기초관련) AlgorithmMD > TIL

---
+++ 기초 이론정리/예제 코드/linkedlist.py
@@ -0,0 +1,107 @@
+# 추상적인 단방향 연결 리스트
+class Node:
+    
+    def __init__(self, item):
+        self.data = item
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = None
+        self.tail = None
+
+
+    def __repr__(self):
+        if self.nodeCount == 0:
+            return 'LinkedList: empty'
+
+        s = ''
+        curr = self.head
+        while curr is not None:
+            s += repr(curr.data)
+            if curr.next is not None:
+                s += ' -> '
+            curr = curr.next
+        return s
+
+
+    def getAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return None
+
+        i = 1
+        curr = self.head
+        while i < pos:
+            curr = curr.next
+            i += 1
+
+        return curr
+
+
+    def insertAt(self, pos, newNode):
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+
+        if pos == 1:
+            newNode.next = self.head
+            self.head = newNode
+
+        else:
+            if pos == self.nodeCount + 1:
+                prev = self.tail
+            else:
+                prev = self.getAt(pos - 1)
+            newNode.next = prev.next
+            prev.next = newNode
+
+        if pos == self.nodeCount + 1:
+            self.tail = newNode
+
+        self.nodeCount += 1
+        return True
+
+
+    def getLength(self):
+        return self.nodeCount
+
+
+    def traverse(self):
+        result = []
+        curr = self.head
+        while curr is not None:
+            result.append(curr.data)
+            curr = curr.next
+        return result
+
+
+    def concat(self, L):
+        self.tail.next = L.head
+        if L.tail:
+            self.tail = L.tail
+        self.nodeCount += L.nodeCount
+
+    def popAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            raise IndexError
+                    
+        prev = self.getAt(pos-1)
+        curr = self.getAt(pos)
+        
+        if pos == 1:
+            if self.nodeCount==1:
+                self.head=None
+                self.tail=None
+            self.head = curr.next
+            curr.next = None
+                                   
+        elif pos == self.nodeCount:
+                self.tail = prev
+                prev.next = None
+        else: 
+            prev.next = curr.next
+            
+        self.nodeCount -= 1
+        return curr.data
---
+++ 프론트엔드 로드맵/README.md
@@ -0,0 +1,11 @@
+# 프론트엔드 로드맵
+## 목표
+1. 부족한 부분 파악 및 학습
+2. 필수 옵션과 선택 옵션 비추천 옵션 정리, 개인적 선택
+
+## 참고 로드맵
+> [로드맵 github](https://github.com/kamranahmedse/developer-roadmap/blob/master/translations/korean/img/frontend.png)
+
+> [로드맵.sh](https://roadmap.sh/)
+
+> [Code.org youtube](https://www.youtube.com/channel/UCJyEBMU1xVP2be1-AoGS1BA)
